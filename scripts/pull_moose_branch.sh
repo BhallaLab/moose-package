9b132f1a719bfe11c1413ab151e29619b82b12dc
Dilawar Singh <dilawars@ncbs.res.in>
2017-04-22 11:45:31 +0530

    Chamcham branch is updated.

--- scripts/pull_moose_branch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-(
-git subtree pull --prefix moose-python \
-    https://github.com/BhallaLab/moose-python-package master --squash
-git subtree pull --prefix moose-gui \
-    https://github.com/BhallaLab/moose-gui-package master --squash
-)
