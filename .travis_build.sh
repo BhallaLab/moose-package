d039f0779967610bd4dfea9f989a32b455ba0969
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-05 12:33:40 +0530

    moogli should not import moogli.

---
+++ .travis_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: .travis_build.sh
+#
+#         USAGE: ./.travis_build.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: Monday 05 March 2018 12:26:32  IST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+set -e -x
+
+mkdir -p _build
+cd _build
+cmake -DCMAKE_INSTALL_PREFIX=/tmp/moogli/usr ..
+make -j`nproc`
+make install
+export PYTHONPATH=/tmp/moogli$(python -c 'import site;print(site.getsitepackages()[-1])')
+python -c 'import moogli;print(moogli.__file__);'
