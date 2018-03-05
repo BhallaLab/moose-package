bb6e8dc734077dae2bdeba51e3676162e35d90e3
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-05 12:10:40 +0530

    Tiny changes to push script (subtree).

--- scripts/push_subtree.sh
+++ scripts/push_subtree.sh
@@ -4,8 +4,6 @@
 CUR_BRANCH=$(git branch | grep \* | cut -d' ' -f2)
 echo "We are on $CUR_BRANCH"
 (
-    git subtree push --prefix moose https://github.com/BhallaLab/moose \
-        $CUR_BRANCH 
-    git subtree push --prefix moogli https://github.com/BhallaLab/moogli \
-        $CUR_BRANCH 
+    git subtree push --prefix moose https://github.com/BhallaLab/moose $CUR_BRANCH 
+    git subtree push --prefix moogli https://github.com/BhallaLab/moogli $CUR_BRANCH 
 )
